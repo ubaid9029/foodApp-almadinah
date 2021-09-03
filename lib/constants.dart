92afe1fe539ee1bc26b15c07d6c8b11158845482
ubaidasim <ubaidasim7417@gmail.com>
2021-09-03 12:43:30 +0500

    foodapp

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color redTheme = Color(0xffA02621);
+const Color yela1 = Colors.yellowAccent;
+const Color yelo2 = Colors.yellow;
+const Gradient redT = LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+    colors: [Color(0xffA02621), Color(0xff4b0205)]);
+
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'constants.dart';
+import 'splash.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  var path = await getApplicationDocumentsDirectory();
+  Hive.init(path.path);
+  await Hive.openBox("favBox");
+  await Hive.openBox("allKeys");
+  await Hive.openBox("addToCart");
+  await Hive.openBox("addKeys");
+  await Hive.openBox("hKeys");
+  await Hive.openBox("hValues");
+  runApp(MyApp());
+  //  Admob.initialize("com.example.food_app1");
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: redTheme,
+        fontFamily: "ubuntu",
+      ),
+      home: Splash(),
+    );
+  }
+}
+// flutter build apk --split-per-abi  --target-platform android-arm,android-arm64,android-x64
